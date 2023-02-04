52ac1858f0260e3eefa8595c571ec805b267d557
Miguelife <115809166+Miguelife@users.noreply.github.com>
2023-02-04 17:23:01 +0100

    .

--- GGJ2022/Assets/ProjectUtils/TopDownMovement/Fighter.cs
+++ GGJ2022/Assets/ProjectUtils/TopDownMovement/Fighter.cs
@@ -4,59 +4,58 @@ using UnityEngine;
 
 namespace ProjectUtils.TopDown2D
 {
-  public class Fighter : MonoBehaviour, IDamageable
-  {
-    public float health = 10;
-    public float maxHealth = 10;
-    public float pushRecoverySpeed = 1;
-
-    [SerializeField] private float immuneTime = 1;
-    private float _lastImmune;
-
-    protected Vector3 pushDirection;
-    protected Vector3 dashDirection;
-
-    public void ReceiveDamage(Damage dmg)
+    public class Fighter : MonoBehaviour, IDamageable
     {
+        [Header("Fighting")]
+        public float health = 10;
+        public float maxHealth = 10;
 
-      if (Time.time - _lastImmune > immuneTime)
-      {
-               
-        _lastImmune = Time.time;
-        health -= dmg.damageAmount;
-        pushDirection = (transform.position - dmg.origin).normalized * dmg.pushForce;
+        [SerializeField] private float immuneTime = 1;
+        private float _lastImmune;
 
-        if (health <= 0)
+        protected Vector3 pushDirection;
+
+        public void ReceiveDamage(Damage dmg)
         {
-          health = 0;
-u        }
+            if (Time.time - _lastImmune > immuneTime)
+            {
+                _lastImmune = Time.time;
+                health -= dmg.damageAmount;
+                pushDirection = (transform.position - dmg.origin).normalized * dmg.pushForce;
 
-      }
+                if (health <= 0)
+                {
+                    health = 0;
+                    Death();
+                }
+
+                StartCoroutine(ImmuneDisplay());
+            }
+        }
+
+        private IEnumerator ImmuneDisplay()
+        {
+            float elapsedTime = 0;
+            SpriteRenderer spriteRenderer = GetComponent<SpriteRenderer>();
+            if (spriteRenderer == null) spriteRenderer = GetComponentInChildren<SpriteRenderer>();
+            Color originalColor = spriteRenderer.color;
+
+            while (elapsedTime < immuneTime)
+            {
+                spriteRenderer.color = new Color(255, 255, 255, 0);
+                yield return new WaitForSeconds(immuneTime / 10);
+                spriteRenderer.color = originalColor;
+                yield return new WaitForSeconds(immuneTime / 10);
+                elapsedTime += immuneTime / 5;
+
+            }
+
+            spriteRenderer.color = originalColor;
+        }
+
+        protected virtual void Death()
+        {
+            Destroy(gameObject);
+        }
     }
-
-    private IEnumerator ImmuneDisplay()
-    {
-      float elapsedTime = 0;
-      SpriteRenderer spriteRenderer = GetComponent<SpriteRenderer>();
-      if (spriteRenderer == null) spriteRenderer = GetComponentInChildren<SpriteRenderer>();
-      Color originalColor = spriteRenderer.color;
-
-      while (elapsedTime < immuneTime)
-      {
-        spriteRenderer.color = new Color(255, 255, 255, 0);
-        yield return new WaitForSeconds(immuneTime / 10);
-        spriteRenderer.color = originalColor;
-        yield return new WaitForSeconds(immuneTime / 10);
-        elapsedTime += immuneTime / 5;
-
-      }
-
-      spriteRenderer.color = originalColor;
-    }
-
-    protected virtual void Death()
-    {
-      Destroy(gameObject);
-    }
-  }
 }
